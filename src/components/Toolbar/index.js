821c01c44043ca5471aedcaa6530098e02bcb7e3
Air2air <todd@dunningmarketing.com>
2022-03-13 14:34:44 -0700

    No Husky

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -3,6 +3,7 @@ import {ImFontSize, ImFont} from 'react-icons/im'
 import rabbit from '../../assets/images/rabbit.svg'
 import turtle from '../../assets/images/turtle.svg'
 import './index.scss'
+import '../../scss/controls.scss'
 
 const Toolbar = () => (
   <>
@@ -14,10 +15,10 @@ const Toolbar = () => (
         <ImFont className="icon" size={32} />
       </div>
       <div className="control">
-        <img src={turtle} className="svg" alt="Slow" />
+        <img src={turtle} className="svg-icon" alt="Slow" />
       </div>
       <div className="control">
-        <img src={rabbit} className="svg" alt="Fast" />
+        <img src={rabbit} className="svg-icon" alt="Fast" />
       </div>
     </div>
   </>
